c6b6ea232fdaee7c8c061ca2a05b2d31b178b4ef
Neil Churaman <nc2263@nyu.edu>
2022-03-20 16:33:45 -0400

    new changes to search_page

--- front-end/src/search_rec/main.js
+++ front-end/src/search_rec/main.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './style.css';
-import Header from './header.js';
-import search_bar from './search_bar.js';
+import '.\style.css;
+import Header from '.\header.js';
+import search_bar from '.\search_bar.js';
+import recommended_pictures from '.\recommended_pictures.js';
+import candies from '.\candies.jpg';
+import cereals from '.\cereals.png';
+import frozen_foods from '.\frozen-foods.jpg';
 const search_rec=()=>{
     return (
-    <div className='container'
+    <div className='container'>
         <Header name="Search Page"/>
         <search_bar/>
+        <div className='pictures_captions'>
+            <recommended_pictures name='candies' image={candies}/>
+            <recommended_pictures name='cereals' image={cereals}/>
+            <recommended_pictures name = 'frozen foods' image={frozen_foods}/>
+        </div>
     </div>   
     );
 }
