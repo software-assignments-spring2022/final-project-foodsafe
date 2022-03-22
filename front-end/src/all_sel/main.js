f78218e4f966fdd5d9b4fca710130e1ecc2eabd6
Jake Kallman <jk6523@stern.nyu.edu>
2022-03-21 20:14:35 -0400

    allergycheckbox

--- front-end/src/all_sel/main.js
+++ front-end/src/all_sel/main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import App from "./allergy";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 const all_sel=()=>{
     return (
     <a>
-        <h2>Allergy selection - jake (to be done)!</h2>
+        <h2>Allergy selection</h2>
+        <App />
     </a>
     
     );
