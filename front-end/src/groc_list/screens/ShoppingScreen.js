56c847090aeaf0f7a7d43be0931dc107c2488836
Jake Kallman <jk6523@stern.nyu.edu>
2022-04-02 14:09:27 -0400

    fixedapis

--- front-end/src/groc_list/screens/ShoppingScreen.js
+++ front-end/src/groc_list/screens/ShoppingScreen.js
@@ -1,12 +1,12 @@
-import products from "../data";
+import { useEffect, useState } from "react";
 import "./ShoppingScreen.css";
 import FoodCard from '../Components/FoodCard';
 import {BASE_URL, LIST_PRODUCTS} from '../../apis/index'
-import { useEffect, useState } from "react";
 
 const ShoppingScreen = () => {
 
   const [productsList, updateProductsList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchProducts();
@@ -14,16 +14,18 @@ const ShoppingScreen = () => {
 
   const fetchProducts = async () => {
     let apis = `${BASE_URL}/${LIST_PRODUCTS}`;
+    setLoading(true); //loading on
     let response = await fetch(apis);
     response = await response.json();
     updateProductsList(response.data);
+    setLoading(false); //loading off
   }
 
   return (
     <div className='products__wrapper'>
-      {productsList.map((product) => (
+      {loading==false ? productsList.map((product) => (
         <FoodCard key={product.id} product={product} />
-      ))}
+      )) : 'Loading...'}
     </div>
   );
 };
