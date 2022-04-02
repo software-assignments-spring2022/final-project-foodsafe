6dcfc16dfa0104fa676baa4571857b2b34106026
Jake Kallman <jk6523@stern.nyu.edu>
2022-04-02 13:49:40 -0400

    jakeproductsbackend

--- front-end/src/groc_list/screens/ShoppingScreen.js
+++ front-end/src/groc_list/screens/ShoppingScreen.js
@@ -1,11 +1,27 @@
 import products from "../data";
 import "./ShoppingScreen.css";
 import FoodCard from '../Components/FoodCard';
+import {BASE_URL, LIST_PRODUCTS} from '../../apis/index'
+import { useEffect, useState } from "react";
 
 const ShoppingScreen = () => {
+
+  const [productsList, updateProductsList] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  },[]);
+
+  const fetchProducts = async () => {
+    let apis = `${BASE_URL}/${LIST_PRODUCTS}`;
+    let response = await fetch(apis);
+    response = await response.json();
+    updateProductsList(response.data);
+  }
+
   return (
     <div className='products__wrapper'>
-      {products.map((product) => (
+      {productsList.map((product) => (
         <FoodCard key={product.id} product={product} />
       ))}
     </div>
