3ec1d3fdc861d49ddd50a8cff2e117d800895b91
JefferyWCG <cw3736@nyu.edu>
2022-04-02 22:51:09 -0400

    Allergy back-end

---
+++ back-end/routes/allergy.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+let Allergies=[];
+//['Milk','Egg','Fish','Crustacean shellfish','Tree Nut','Peanut','Wheat','SoyBean'];
+/*
+const ifAllergic={
+    'Milk':false,
+    'Egg':false,
+    'Fish':false,
+    'Crustacean shellfish':false,
+    'Tree Nut':false,
+    'Peanut':false,
+    'Wheat':false,
+    'SoyBean':false,
+}*/
+
+
+//this is used for the introduction page (only once)
+//expect to receive an array of number
+
+
+router.get('/',(req,res)=>{
+    res.json(Allergies);
+})
+
+//expect to receive an array of number
+router.post('/',async(req,res)=>{
+    Allergies=req.body.newAllergies;
+    console.log(Allergies);
+})
+
+module.exports = router;
+
