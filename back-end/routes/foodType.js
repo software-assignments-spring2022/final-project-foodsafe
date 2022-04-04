f6d3d585745d9b6dde384b07e6fed7bf4acf107d
JefferyWCG <98352935+JefferyWCG@users.noreply.github.com>
2022-04-03 22:46:08 -0400

    create FoodType route

---
+++ back-end/routes/foodType.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+let foods=['foo','bar']
+let searchQuery;
+router.post('/',(req,res)=>{
+    res.json(foods);
+})
+
+
+router.post('/',(req,res)=>{
+    queryString=req.body.searchQuery;
+})
+
+
+module.exports = router;
