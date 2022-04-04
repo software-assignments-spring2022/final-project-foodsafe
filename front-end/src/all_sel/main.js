79ade68c1c69ece9c73370bb8bbb378813c91bd5
Vincent Cheng <chl611@nyu.edu>
2022-04-04 17:17:37 -0400

    center text

--- front-end/src/all_sel/main.js
+++ front-end/src/all_sel/main.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import App from "./allergy";
+import "./all_sel.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 const all_sel=()=>{
     return (
     <a>
-        <h2>Allergy selection</h2>
+        <h2 className = "title">Allergy selection</h2>
         <App />
     </a>
     
