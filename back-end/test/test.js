2a5def5a2aa8c16a26c3e43f175bbb3fee2773e3
Jake Kallman <jk6523@stern.nyu.edu>
2022-04-03 20:21:08 -0400

    jake unit testing and port config

---
+++ back-end/test/test.js
@@ -0,0 +1,29 @@
+var expect = require('chai').expect;
+var request = require('request');
+var {PORT} = require('../config/globalVariables');
+
+describe("Status and Content",function(){
+
+    describe("Listing API", function(){
+        it('status',function(done){
+            request(`http://localhost:${PORT}/list-products`,function(error, response, body){
+                expect(response.statusCode).to.equal(200);
+                done();
+            })
+        })
+
+        it("Check Response Type",function(done){
+            request(`http://localhost:${PORT}/list-products`,function(error, response, body){
+                expect(JSON.parse(response.body).data).to.be.an("array");
+                done();
+            })
+        })
+
+        it("Content Length",function(done){
+            request(`http://localhost:${PORT}/list-products`,function(error, response, body){
+                expect(JSON.parse(response.body).data.length).greaterThan(0);
+                done();
+            })
+        })
+    })
+})
