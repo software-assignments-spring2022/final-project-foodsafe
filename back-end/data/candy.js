62cb3ae70dfb23bc56d216d1ae4e12ef243eb691
Vincent Cheng <chl611@nyu.edu>
2022-04-06 22:15:29 -0400

    save data

---
+++ back-end/data/candy.js
@@ -0,0 +1,31 @@
+const candies = [
+    {
+      _id: "1",
+      name: "Coke Candy",
+      image:
+        "https://pics.drugstore.com/prodimg/436319/900.jpg",
+  
+      price: 3.00,
+    },
+    {
+      _id: "2",
+      name: "Gummy Bear",
+      image:
+        "https://pics.drugstore.com/prodimg/413256/900.jpg",
+  
+      price: 3.50,
+    },
+    {
+      _id: "3",
+      name: "Skittles",
+      image:
+        "https://www.skittles.com/sites/g/files/fnmzdf586/files/migrate-product-files/knafpht4p3g6tqlktjdu.png",
+  
+      price: 4.00,
+    },
+    
+    
+    
+  ];
+  
+  module.exports = candies;
