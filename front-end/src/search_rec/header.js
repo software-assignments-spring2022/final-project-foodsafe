0f42dd48d0bc3b43bdb233747b8ad174f204643a
Vincent Cheng <chl611@nyu.edu>
2022-04-07 23:20:27 -0400

    update

--- front-end/src/search_rec/header.js
+++ front-end/src/search_rec/header.js
@@ -1,9 +1,11 @@
 import Change_allergy_button from './change_allergy_button';
+import { useNavigate } from 'react-router-dom';
+
 const Header = ({name})=>{
 	return (
 		<header className='header'>
 			<h1>{name}</h1>
-			<Change_allergy_button />
+			
 		</header>
 	);
 }
